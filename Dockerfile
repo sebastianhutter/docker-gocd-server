fdd032aeff7e9a2fb0406f9bc8638adc519a3916
Sebastian Hutter <mail@sebastian-hutter.ch>
2017-03-25 17:02:09 +0100

    Migrate to new gocd-server image

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,27 @@
-FROM gocd/gocd-server
+FROM gocd/gocd-server:v17.3.0
 
 # plugin versions to install
 #ARG PLUGIN_GITHUB_OAUTH_LOGIN=2.3
 ARG PLUGIN_YAML_CONFIG_PLUGIN=0.4.0
 
+ENV GO_HOME=/home/go
+ENV GO_DATA=/godata
+
 # install gettext and jq for our entrypoint
-RUN apt-get update \
-  && apt-get install -y gettext jq \
-  && rm -rf /var/lib/apt/lists/*
+RUN apk add --no-cache curl jq gettext
 
 # install additional plugins we need
-RUN cd /var/lib/go-server/plugins/external \
+RUN mkdir -p ${GO_DATA}/plugins/external \
+  && cd ${GO_DATA}/plugins/external \
 #  && curl -LO https://github.com/gocd-contrib/gocd-oauth-login/releases/download/v${PLUGIN_GITHUB_OAUTH_LOGIN}/github-oauth-login-${PLUGIN_GITHUB_OAUTH_LOGIN}.jar \
-  && curl -LO https://github.com/tomzo/gocd-yaml-config-plugin/releases/download/${PLUGIN_YAML_CONFIG_PLUGIN}/yaml-config-plugin-${PLUGIN_YAML_CONFIG_PLUGIN}.jar
+  && curl -LO https://github.com/tomzo/gocd-yaml-config-plugin/releases/download/${PLUGIN_YAML_CONFIG_PLUGIN}/yaml-config-plugin-${PLUGIN_YAML_CONFIG_PLUGIN}.jar \
+  && chown -R go:go ${GO_DATA}/plugins
 
 # add cruise config xml and custom entrypoint script
-ADD cruise-config.xml /cruise-config.xml
-ADD docker-entrypoint.sh /docker-entrypoint.sh
-ADD ssh.config /var/go/.ssh/config
-RUN chmod +x /docker-entrypoint.sh \
-  && chown -R go:go /var/go/.ssh
+ADD build/cruise-config.xml /cruise-config-custom.xml
+ADD build/docker-entrypoint.sh /docker-entrypoint-custom.sh
+ADD build/ssh.config ${GO_HOME}/.ssh/config
+RUN chmod +x /docker-entrypoint-custom.sh \
+  && chown -R go:go ${GO_HOME}/.ssh
 
-ENTRYPOINT ["/docker-entrypoint.sh"]
+ENTRYPOINT ["/docker-entrypoint-custom.sh"]
--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -1,18 +1,22 @@
-#!/bin/bash
+#!/bin/sh
 
 # first we need to grep the gocd serverid from the cruise-xml config
 # we dont want that one to be regenerated every single time we do
 # a restart of the docker container
-[ -z "$GOCD_SERVERID" ] && export GOCD_SERVERID=$(grep -o 'serverId=".*"' /etc/go/cruise-config.xml | awk '{ gsub("\"",""); gsub("serverId=",""); print $1 }')
+
+# cruise config
+cruise_xml=${GO_DATA}/config/cruise-config.xml
+
+[ -z "$GOCD_SERVERID" ] && export GOCD_SERVERID=$(grep -o 'serverId=".*"' ${cruise_xml} | awk '{ gsub("\"",""); gsub("serverId=",""); print $1 }')
 
 # replace configuration variables in the cruise-config.xml
-envsubst < "/cruise-config.xml" > "/etc/go/cruise-config.xml"
-chown go:go "/etc/go/cruise-config.xml"
+envsubst < "/cruise-config.xml" > "${cruise_xml}"
+chown go:go "${cruise_xml}"
 
 # now if the cruise-config.xml file already exists it is ok to have the serverId field defined.
 # if the config file doesnt exist the template will create an empty serverId="" field which
 # we need to remove completely or the gocd server won't generate a proper serverid
-sed -i 's/serverId=""//' /etc/go/cruise-config.xml
+sed -i 's/serverId=""//' ${cruise_xml}
 
 # next step is to add our repositories which we want to use for the build pipelines
 # the idea is to have a build pipeline per repo. the yaml pipeline plugin supports this by
@@ -25,7 +29,7 @@ if [ -n "$GOCD_YAML_REPOSITORIES" ]; then
     # http://stackoverflow.com/questions/22497246/insert-multiple-lines-into-a-file-after-specified-pattern-using-shell-script
     echo -e "<config-repo plugin=\"yaml.config.plugin\">\n  <git url=\"git@github.com:${r}.git\" />\n</config-repo>" >> ${TMPREPO}
   done
-  sed -i "/<!-- CONFIGREPOS -->/r ${TMPREPO}" /etc/go/cruise-config.xml
+  sed -i "/<!-- CONFIGREPOS -->/r ${TMPREPO}" ${cruise_xml}
   rm ${TMPREPO}
 fi
 
@@ -38,10 +42,10 @@ ACCESS_TOKEN=$(curl -X POST \
      ${VAULT_SERVER}/v1/auth/approle/login | jq -r .auth.client_token)
 
 # write the private key file
-curl -X GET -H "X-Vault-Token:${ACCESS_TOKEN}" ${VAULT_SERVER}/v1/${VAULT_SECRET_GITHUB_KEY} | jq -r .data.value > /var/go/.ssh/id_rsa
+curl -X GET -H "X-Vault-Token:${ACCESS_TOKEN}" ${VAULT_SERVER}/v1/${VAULT_SECRET_GITHUB_KEY} | jq -r .data.value > ${GO_HOME}/.ssh/id_rsa
 unset OUTPUT
-chown go:go /var/go/.ssh/id_rsa
-chmod 0600 /var/go/.ssh/id_rsa
+chown go:go ${GO_HOME}/.ssh/id_rsa
+chmod 0600 ${GO_HOME}/.ssh/id_rsa
 
 # after configuringt the server execute the original entrypoint
-exec /sbin/my_init
+exec /docker-entrypoint.sh
